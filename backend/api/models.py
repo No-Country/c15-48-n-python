452e6ffb59d5c81178fb1eb9c35a88750adb3443
suspiciousRaccoon <spablocabral@gmail.com>
2023-11-28 22:02:40 -0300

    feat(models): add AbstractUser

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,11 @@ from django.utils.translation import gettext_noop
 from imagekit.processors import ResizeToFill
 from imagekit.models import ImageSpecField
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
 
 
 class Pet(models.Model):
