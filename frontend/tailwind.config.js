2b59a53a067bbcf5ee84e586753adf9c96ac3531
abril <rodriguezabbril9@gmail.com>
2023-11-29 23:13:36 -0300

    tailwind colors and font config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
+    colors: {
+      'social-pink': '#F62E8E',
+      'social.blue': '#2E8AF6',
+      'dark-black': '#181A1C',
+      'light-white': '#ECEBED',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'gray': '#323436',
+      'light-gray': '#727477',
+    },
+    fontFamily: {
+      custom: ['HK Grotesk', 'sans-serif']
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 
