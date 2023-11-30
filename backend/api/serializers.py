14f51fb9a266b4a77731191d6a79cb9a2adbe66e
Nahuel <nahuel.94.arrieta@gmail.com>
2023-11-29 21:56:04 -0300

    fix(serializer/PetSerializer): fix species

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import *
 
 class PetSerializer(serializers.ModelSerializer):
+    species = serializers.ChoiceField(choices=Pet.PetSpecies.choices)
     class Meta:
         model: Pet
         fields = '__all__'
-
