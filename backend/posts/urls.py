061e0abb168294776a12fc74b9f210a77e345da0
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-05 20:24:01 -0300

    fix(viewsets/posts): rename api to views

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,18 +1,17 @@
-from django.urls import include,path
 from rest_framework import routers
-from .api import (
+from .views import (
     PostViewSet,
     PostImageViewSet,
     PostVideoViewSet,
     CommentViewSet,
-    LikeViewSet
+    LikeViewSet,
 )
 
 router = routers.DefaultRouter()
-router.register('api/posts', PostViewSet, 'posts')
-router.register('api/images', PostImageViewSet, 'images')
-router.register('api/videos', PostVideoViewSet, 'videos')
-router.register('api/comments', CommentViewSet, 'comments')
-router.register('api/likes', LikeViewSet, 'likes')
+router.register("api/posts", PostViewSet, "posts")
+router.register("api/images", PostImageViewSet, "images")
+router.register("api/videos", PostVideoViewSet, "videos")
+router.register("api/comments", CommentViewSet, "comments")
+router.register("api/likes", LikeViewSet, "likes")
 
 urlpatterns = router.urls
