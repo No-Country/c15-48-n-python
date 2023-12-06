35de9bc04a16265f07e7ec99b6853058ec0a7749
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-05 21:04:37 -0300

    feat(viewsets/accounts): add permissions

---
+++ backend/accounts/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user:
+            if request.user.is_superuser:
+                return True
+            else:
+                return obj.owner == request.user
+        else:
+            return False
