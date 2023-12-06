3bdd31475042c78409612bde48bfa075ab54ee02
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-05 21:05:04 -0300

    feat(accounts/views): add viewsets

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from .permissions import IsOwner, IsOwnerOrReadOnly
+from rest_framework import viewsets, permissions
+from .models import User, Follower, Blocker, Pet
+from .serializers import (
+    AccountSerializer,
+    FollowerSerializer,
+    BlockerSerializer,
+    PetSerializer,
+)
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
+    serializer_class = AccountSerializer
+
+
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
+    serializer_class = PetSerializer
+
+
+class FollowerViewSet(viewsets.ModelViewSet):
+    queryset = Follower.objects.all()
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
+    serializer_class = FollowerSerializer
+
+
+class BlockerViewSet(viewsets.ModelViewSet):
+    queryset = Blocker.objects.all()
+    permission_classes = (IsOwner,)
+    serializer_class = BlockerSerializer
