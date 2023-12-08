c747f46062b9502398ea9db1749b2fdecea7c993
JoelvProg <125759787+JoelvProg@users.noreply.github.com>
2023-12-07 22:57:00 -0500

    efecto a iconos

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,15 +2,26 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>
-      
-      <footer className="h-16 flex items-center justify-around px-4 border-t-2 border-gray-300">
-        <NavLink to="/"><img src="./src/assets/feed.png" alt="logo de algo" /></NavLink>
-        <NavLink to="/"><img src="./src/assets/Search.png" alt="logo de algo" /></NavLink>
-        <NavLink to="/" className="bg-social-pink rounded-xl"><img src="./src/assets/Plus.png" alt="logo de algo" /></NavLink>
-        <NavLink to="/"><img src="./src/assets/Alert.png" alt="logo de algo" /></NavLink>
-        <NavLink to="/"><img src="./src/assets/Profile maskota.png" alt="logo de algo" /></NavLink>
-      </footer>
+    <div className="h-16 flex items-center justify-around px-4 border-t-2 border-gray-300">
+
+        <NavLink to="/home"><img className="active:opacity-100 opacity-20 hover:opacity-100 focus:opacity-100" src="./src/assets/home 2.svg" alt="logo de algo"/></NavLink>
+        <NavLink to="/explore"><img className="active:opacity-100 opacity-20 hover:opacity-100 focus:opacity-100" src="./src/assets/search.svg" alt="logo de algo" /></NavLink>
+        <NavLink to="/publish" className="bg-social-pink rounded-xl"><img className="hover:opacity-60" src="./src/assets/Plus.svg" alt="logo de algo" /></NavLink>
+        <NavLink to="/notifications"><img className="active:opacity-100 opacity-20 hover:opacity-100 focus:opacity-100" src="./src/assets/Alert.svg" alt="logo de algo" /></NavLink>
+        <NavLink to="/profile"><img className="active:opacity-100 opacity-20 hover:opacity-100 focus:opacity-100" src="./src/assets/Group 1.svg" alt="logo de algo" /></NavLink>
+            
+    
     </div>
+
+    
+      
+      
+    
+
+
+
+
+
+
   );
 }
