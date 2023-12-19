bc6c2b684f240fb4ef1193ecaca50e2ba0250b96
suspiciousRaccoon <127566947+suspiciousRaccoon@users.noreply.github.com>
2023-12-18 21:17:03 -0300

    fix(core/urls): add missing space

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-   path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
