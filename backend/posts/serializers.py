bf2d2c884dae02d4a4db7aed66a1e3f4c754ebe0
Nahuel <nahuel.94.arrieta@gmail.com>
2023-12-20 22:15:32 -0300

    refactor(posts/serializer): refactor PostSerializer

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,35 +1,21 @@
 from rest_framework import serializers
-from .models import Post, PostImage, Pet, PostVideo, Like, Comment
+from .models import Post, Pet, Like, Comment
 from accounts.serializers import PetAbridgedSerializer
 
 class PostSerializer(serializers.ModelSerializer):
-    pet = PetAbridgedSerializer(many=True, read_only=True)
+    pet = PetAbridgedSerializer(read_only=True)
 
     class Meta:
         model = Post
-        fields = ['pet', 'caption', 'created_at']
+        fields = ['pet', 'image', 'video', 'caption', 'created_at']
         read_only_fields = ('created_at', )
 
 
-class PostImageSerializer(serializers.ModelSerializer):
-    post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = PostImage
-        fields = ['post', 'image', 'alt_text']
-
-
-class PostVideoSerializer(serializers.ModelSerializer):
-    post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = PostVideo
-        fields = ['post', 'video', 'alt_text']
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    pet = PetAbridgedSerializer(many=True, read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    pet = PetAbridgedSerializer(read_only=True)
 
     class Meta:
         model = Comment
@@ -38,7 +24,7 @@ class CommentSerializer(serializers.ModelSerializer):
     
 
 class LikeSerializer(serializers.ModelSerializer):
-    pet = PetAbridgedSerializer(many=True, read_only=True)
+    pet = PetAbridgedSerializer(read_only=True)
     post = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
