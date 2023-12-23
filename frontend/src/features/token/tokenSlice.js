79fa364c4484e0c77e672135866ec6257a7f41bc
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-23 20:32:10 -0300

    feat(tokenSlice): add tokenSlice endpoints

--- frontend/src/features/token/tokenSlice.js
+++ frontend/src/features/token/tokenSlice.js
@@ -2,11 +2,34 @@ import { apiSlice } from "../api/apiSlice.js";
 
 const extendedTokenApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getToken: builder.query({
-      // This endpoint doesn't actually exist, it's only temporary
-      query: () => "/token",
+    createToken: builder.mutation({
+      query: (userData) => ({
+        url: "/token",
+        method: "POST",
+        body: userData,
+      }),
+      providesTagsTags: ["token"],
+    }),
+    verifyToken: builder.mutation({
+      query: (token) => ({
+        url: "/token",
+        method: "POST",
+        body: token,
+      }),
+    }),
+    refreshToken: builder.mutation({
+      query: (refresh_token) => ({
+        url: "/token",
+        method: "POST",
+        body: refresh_token,
+      }),
+      invalidatesTags: ["token"],
     }),
   }),
 });
 
-export const { useGetTokenQuery } = extendedTokenApiSlice;
+export const {
+  useCreateTokenMutation,
+  useVerifyTokenMutation,
+  useRefreshTokenMutation,
+} = extendedTokenApiSlice;
