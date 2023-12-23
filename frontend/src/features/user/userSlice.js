65ec8e94fe059fe48856dcf87e435e2a6602e072
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-23 20:08:56 -0300

    feat(userSlice): add user reducers

--- frontend/src/features/user/userSlice.js
+++ frontend/src/features/user/userSlice.js
@@ -3,9 +3,59 @@ import { apiSlice } from "../api/apiSlice.js";
 const extendedUserApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
-      query: () => "/users",
+      query: () => "/user/",
+      providesTags: (result, _error, _arg) => [
+        "users",
+        ...result.map((user) => ({ type: "user", user: user.username })),
+      ],
+    }),
+    getUser: builder.query({
+      query: (username) => ({
+        url: `user/${username}/`,
+      }),
+      providesTags: (_result, _error, arg) => [{ type: "user", user: arg }],
+    }),
+    createNewUser: builder.mutation({
+      query: (userData) => ({
+        url: "/user/",
+        method: "POST",
+        body: userData,
+      }),
+      providesTags: (result, _error, _arg) => [
+        { type: "user", user: result.username },
+      ],
+    }),
+    updateUser: builder.mutation({
+      query: (userData) => ({
+        url: `/user/${userData.username}`,
+        method: "PUT",
+        body: userData,
+      }),
+      invalidatesTags: (_result, _error, arg) => [
+        {
+          type: "user",
+          user: arg.username,
+        },
+      ],
+    }),
+    deleteUser: builder.mutation({
+      query: (username) => ({
+        url: `user/${username}`,
+      }),
+      invalidatesTags: (_result, _error, arg) => [
+        {
+          type: "user",
+          user: arg.username,
+        },
+      ],
     }),
   }),
 });
 
-export const { useGetUsersQuery } = extendedUserApiSlice;
+export const {
+  useGetUsersQuery,
+  useGetUserQuery,
+  useCreateNewUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = extendedUserApiSlice;
