61f76b112ce062d0accdfc00d082be6d27ee30e7
abril <rodriguezabbril9@gmail.com>
2023-12-23 20:59:57 -0300

    feat: add petSlice

--- frontend/src/features/pet/petSlice.js
+++ frontend/src/features/pet/petSlice.js
@@ -4,8 +4,59 @@ const extendedPetApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getPets: builder.query({
       query: () => "/pet",
+      providesTags: (result, _error, _arg) => [
+        "pets",
+        ...result.map((pet) => ({ type: "pet", pet: pet.nick })),
+      ],
+    }),
+    getPet: builder.query({
+      query: (nick) => ({
+        url: `pet/${nick}/`,
+      }),
+      providesTags: (_result, _error, arg) => [{ type: "pet", pet: arg }],
+    }),
+    createNewPet: builder.mutation({
+      query: (petData) => ({
+        url: "/pet/",
+        method: "POST",
+        body: petData,
+      }),
+      providesTags: (result, _error, _arg) => [
+        { type: "pet", pet: result.nick },
+      ],
+    }),
+    updatePet: builder.mutation({
+      query: (petData) => ({
+        url: `/pet/${petData.nick}`,
+        method: "PUT",
+        body: petData,
+      }),
+      invalidatesTags: (_result, _error, arg) => [
+        {
+          type: "pet",
+          pet: arg.nick,
+        },
+      ],
+    }),
+    deleteUser: builder.mutation({
+      query: (username) => ({
+        url: `user/${username}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (_result, _error, arg) => [
+        {
+          type: "pet",
+          pet: arg.nick,
+        },
+      ],
     }),
   }),
 });
 
-export const { useGetPetsQuery } = extendedPetApiSlice;
+export const {
+  useGetPetssQuery,
+  useGetPetQuery,
+  useCreateNewPetMutation,
+  useUpdatePetMutation,
+  useDeletePetMutation,
+} = extendedPetApiSlice;
