935cc3a34146968952b669291a1161e09f081558
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-23 17:40:52 -0300

    feat(blockerSlice): add extendedBlockerApiSlice

---
+++ frontend/src/features/blocker/blockerSlice.js
@@ -0,0 +1,11 @@
+import { apiSlice } from "../api/apiSlice.js";
+
+const extendedBlockerApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getBlockers: builder.query({
+      query: () => "/blocker",
+    }),
+  }),
+});
+
+export const { useGetBlockersQuery } = extendedUserApiSlice;
