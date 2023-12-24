f29e56d6c174a71fa31b08546ac4e90c9f815db6
abril <rodriguezabbril9@gmail.com>
2023-12-23 21:41:16 -0300

    feat: add petSlice and postsSlice

--- frontend/src/features/posts/postsSlice.js
+++ frontend/src/features/posts/postsSlice.js
@@ -4,8 +4,45 @@ const extendedPostsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: () => "/posts",
+      providesTags: (result, _error, _arg) => [
+        ...result.map((post) => ({ type: "post", post: post.id })),
+      ],
+    }),
+    getPost: builder.query({
+      query: (id) => ({
+        url: `posts/${id}/`,
+      }),
+      providesTags: (_result, _error, arg) => [{ type: "post", post: arg }],
+    }),
+    // get posts by specie - (cuando exista)
+    createNewPost: builder.mutation({
+      query: (postData) => ({
+        url: "/posts/",
+        method: "POST",
+        body: postData,
+      }),
+      providesTags: (result, _error, _arg) => [
+        { type: "post", post: result.id },
+      ],
+    }),
+    deletePost: builder.mutation({
+      query: (id) => ({
+        url: `posts/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (_result, _error, arg) => [
+        {
+          type: "post",
+          pet: arg.id,
+        },
+      ],
     }),
   }),
 });
 
-export const { useGetPostsQuery } = extendedPostsApiSlice;
+export const {
+  useGetPostsQuery,
+  useGetPostQuery,
+  useCreateNewPostMutation,
+  useDeletePostMutation,
+} = extendedPostsApiSlice;
