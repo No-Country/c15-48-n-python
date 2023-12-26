808430046043ba3a0a92da8aec42772cd66d9a62
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-26 20:11:26 -0300

    fix(followerSlice): add ending /

--- frontend/src/features/follower/followerSlice.js
+++ frontend/src/features/follower/followerSlice.js
@@ -3,7 +3,7 @@ import { apiSlice } from "../api/apiSlice.js";
 const extendedFollowerApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getFollowers: builder.query({
-      query: () => "/follower",
+      query: () => "/follower/",
     }),
   }),
 });
