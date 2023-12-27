0fe37b35ad8af689b6fa66175a70d525538baf9b
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-26 21:32:54 -0300

    fix(apiSlice): fix token apth

--- frontend/src/services/apiSlice.js
+++ frontend/src/services/apiSlice.js
@@ -3,10 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
+    // Change to production url later
     baseUrl: "http://localhost:8000/api",
   }),
   prepareHeaders: (headers, { getState }) => {
-    const token = getState().token;
+    const token = getState().auth.token;
 
     if (token) {
       headers.set("authorization", `Bearer ${token}`);
