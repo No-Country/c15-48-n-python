b3c8d46262b646847bcf0a1d5fbf1722495de0eb
abril <rodriguezabbril9@gmail.com>
2023-12-27 00:33:58 -0300

    merge

--- frontend/src/views/profilePet/profilePet.jsx
+++ frontend/src/views/profilePet/profilePet.jsx
@@ -16,115 +16,117 @@ const ProfilePet = () => {
   let perfiles = perfiles_mascotas;
   const user = perfiles[1];
   const params = useParams();
-  const navigate = useNavigate();
   const petProfile = perfiles.find((pet) => pet.id === parseInt(params.id));
+  const { data, isLoading, isFetching, isError, isSuccess } = useGetPetQuery(petProfile.username);
+  const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(false);
   const [active, setActive] = useState(false);
-
+  
   const handleMenu = () => {
     active ? setActive(false) : setActive(true);
   };
-
+  
   const handleOpenFollowers = () => {
-    if (isMobile) {
+    if(isMobile){
       navigate(`/followers/${params.id}`);
     } else {
       console.log("desktop");
       setOpenModal(true);
     }
   };
-
+  
   const handleCloseFollowers = () => {
     setOpenModal(false);
-  };
-
+  }
+  
   if (!petProfile) {
     return (
-      <h2 className='font-custom text-white font-bold text-3xl text-center m-10'>
+      <h2 className="font-custom text-white font-bold text-3xl text-center m-10">
         Perfil no encontrado
       </h2>
     );
   }
   // placeholder publis
   let gatosInfo = gatos;
-
+  
+  if (isError) {
+    console.log()
+  }
+  if (isSuccess) {
+    console.log("QUERY: ", data)
+  }
   return (
-    <div className='w-full flex flex-col items-center overflow-hidden'>
-      <div className='flex justify-between px-8 pt-8 md:max-w-4xl w-full'>
+    <div className="w-full flex flex-col items-center overflow-hidden">
+      <div className="flex justify-between px-8 pt-8 md:max-w-4xl w-full">
         <GoBackButton
-          className='w-4 h-4'
+          className="w-4 h-4"
           img={arrowLeftIcon}
-          alt='flecha hacia la izquierda para volver'
+          alt="flecha hacia la izquierda para volver"
         />
         {params.id != user.id && (
           <button onClick={handleMenu}>
-            <img src={DotsVertical} alt='menú de tres puntos' />
+            <img src={DotsVertical} alt="menú de tres puntos" />
           </button>
         )}
         {active && (
-          <div className='font-custom text-white bg-dark-gray absolute px-4 py-3 rounded-xl right-6 top-14 w-28 text-xs text-center'>
-            <button className='flex items-center'>
-              <img className='mr-2 w-4' src={banIcon} alt='Ícono de bloquear' />
+          <div className="font-custom text-white bg-dark-gray absolute px-4 py-3 rounded-xl right-6 top-14 w-28 text-xs text-center">
+            <button className="flex items-center">
+              <img className="mr-2 w-4" src={banIcon} alt="Ícono de bloquear" />
               Bloquear
             </button>
           </div>
         )}
       </div>
-      <header className='font-custom md:max-w-4xl'>
-        <div className='flex flex-col justify-center text-center text-white'>
+      <header className="font-custom md:max-w-4xl">
+        <div className="flex flex-col justify-center text-center text-white">
           <img
-            className='rounded-full w-36 h-36 border-solid border-2 p-1 m-auto my-4 border-white'
+            className="rounded-full w-36 h-36 border-solid border-2 p-1 m-auto my-4 border-white"
             src={petProfile.profile}
-            alt='foto de perfil'
+            alt="foto de perfil"
           />
-          <h2 className='font-bold text-lg'>{petProfile.name}</h2>
-          <p className='text-xs font-semibold text-light-gray mb-1.5'>
+          <h2 className="font-bold text-lg">{petProfile.name}</h2>
+          <p className="text-xs font-semibold text-light-gray mb-1.5">
             @{petProfile.username}
           </p>
-          <p className='text-sm font-semibold mb-1.5'>
+          <p className="text-sm font-semibold mb-1.5">
             Nací el: {petProfile.date}
           </p>
-          <div className='text-sm mb-12 flex justify-center'>
-            <p className='pr-1'>Mi dueño es:</p>
-            <NavLink to={`${petProfile.human}`} className='font-semibold pr-1'>
+          <div className="text-sm mb-12 flex justify-center">
+            <p className="pr-1">Mi dueño es:</p>
+            <NavLink to={`${petProfile.human}`} className="font-semibold pr-1">
               {petProfile.human}
             </NavLink>
-            <img className='mb-' src={humanIcon} alt='icono perfil de humano' />
+            <img className="mb-" src={humanIcon} alt="icono perfil de humano" />
           </div>
         </div>
       </header>
-      <div className='flex justify-evenly mx-2 md:max-w-4xl w-full'>
-        <div className='flex flex-col font-custom text-sm font-semibold'>
-          <button onClick={handleOpenFollowers} className='flex flex-col'>
-            <span className='text-white'>{petProfile.followers}</span>
-            <span className='text-light-gray'>Seguidores</span>
+      <div className="flex justify-evenly mx-2 md:max-w-4xl w-full">
+        <div className="flex flex-col font-custom text-sm font-semibold">
+          <button onClick={handleOpenFollowers} className="flex flex-col">
+            <span className="text-white">{petProfile.followers}</span>
+            <span className="text-light-gray">Seguidores</span>
           </button>
         </div>
-        <div className='flex flex-col font-custom text-sm font-semibold'>
-          <button onClick={handleOpenFollowers} className='flex flex-col'>
-            <span className='text-white'>{petProfile.followed}</span>
-            <span className='text-light-gray'>Seguidos</span>
+        <div className="flex flex-col font-custom text-sm font-semibold">
+          <button onClick={handleOpenFollowers} className="flex flex-col">
+            <span className="text-white">{petProfile.followed}</span>
+            <span className="text-light-gray">Seguidos</span>
           </button>
         </div>
         {openModal === true && (
-          <FollowersModal
-            userData={petProfile}
-            handleClose={handleCloseFollowers}
-          />
+          <FollowersModal userData={petProfile} handleClose={handleCloseFollowers} />
         )}
         <FollowButton user={user} paramsId={params.id} />
       </div>
 
-      <div className='flex flex-col items-center text-white font-custom text-sm mt-8 mx-6 justify-center md:max-w-4xl w-full'>
+      <div className="flex flex-col items-center text-white font-custom text-sm mt-8 mx-6 justify-center md:max-w-4xl w-full">
         PUBLICACIONES
-        <div className='bg-gradient-to-r from-social-pink to-purple h-1 w-5/8 mt-1 rounded mx-6'></div>
+        <div className="bg-gradient-to-r from-social-pink to-purple h-1 w-5/8 mt-1 rounded mx-6"></div>
       </div>
-      <div className='flex flex-col gap-4 md:max-w-4xl'>
-        {Object.entries(gatosInfo)
-          .filter(([key, value]) => key.includes(params.id))
-          .map(([key, value]) => (
-            <Publication gato={value} key={key} />
-          ))}
+      <div className="flex flex-col gap-4 md:max-w-4xl">
+        {Object.entries(gatosInfo).filter(([key, value]) => key.includes(params.id)).map(([key, value]) => (
+          <Publication gato={value} key={key} />
+        ))}
       </div>
     </div>
   );
