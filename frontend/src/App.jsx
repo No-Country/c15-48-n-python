f3d9d22df7067c4fd712fad1b0857206ae48cc98
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-26 22:03:41 -0300

    refactor(App): change import paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import Register from "./components/registerComponent/register";
+import Register from "./views/register/Register.jsx";
 import HomeComp from "./views/home/homeComp";
 import Login from "./views/login/login";
 import HumanData from "./views/humanData/HumanData";
 import Explore from "./views/explore/explore";
-import Create from "./views/create/Create";
+import CreatePet from "./views/createPet/CreatePet.jsx";
 import CreatePublish from "./views/Publish/CreatePublish";
 import Profile from "./views/profile/profile";
 import Notifications from "./views/notifications/notifications";
@@ -13,6 +13,15 @@ import Followers from "./views/followers/followers.jsx";
 import Publication from "./components/publication.jsx";
 import gatos_info from "./assets/placeholder/gatos_info.js";
 import SharedLayout from "./views/sharedLayout/SharedLayout.jsx";
+import { useCreateTokenMutation } from "./services/tokenSlice.js";
+// import {
+//   useCreateNewUserMutation,
+//   useGetUsersQuery,
+// } from "./features/user/userSlice.js";
+
+// import { useGetUsersQuery } from "./features/api/apiSlice.js";
+import { useEffect } from "react";
+import { useGetUsersQuery } from "./services/userSlice.js";
 
 let gatos = gatos_info;
 const idGatosArray = Object.keys(gatos).map(Number);
@@ -57,7 +66,7 @@ const router = createBrowserRouter([
       },
       {
         path: "crearMaskota",
-        element: <Create />,
+        element: <CreatePet />,
       },
       {
         path: "followers/:id",
@@ -72,6 +81,31 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
+  // const { data, isLoading, error } = useCreateNewUserMutation({
+  //   username: "verylongusername",
+  //   password: "VERYLONGPUSERNAME",
+  //   email: "test@gmail.com",
+  //   first_name: "string",
+  // });
+  const [CreateToken, tokenData] = useCreateTokenMutation();
+  console.log(tokenData);
+
+  useEffect(() => {
+    CreateToken({
+      username: "admin",
+      password: "admin",
+    });
+  }, []);
+
+  const userData = useGetUsersQuery();
+  console.log(userData);
+  // if (isLoading) return <p>Loading</p>;
+  // console.log(data, isFetching, isLoading);
+  // if (data) return <p>{data}</p>;
+  // if (tokenData) {
+  //   console.log(tokenData);
+  // }
+  // console.log(tokenData);
   return <RouterProvider router={router} />;
 };
 
--- frontend/src/views/createPet/CreatePet.jsx
+++ frontend/src/views/createPet/CreatePet.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import InputComp from "../../components/InputComp";
+import InputComp from "../../components/InputComp.jsx";
 import "../../App.css";
-import GoBackButton from "../../components/GoBackButton";
+import GoBackButton from "../../components/GoBackButton.jsx";
 import arrowLeftIcon from "../../assets/icons/arrow_left.svg";
 import profilePlaceholder from "../../assets/profile_placeholder.png";
-import SubmitGradientBtn from "../../components/SubmitGradientBtn";
+import SubmitGradientBtn from "../../components/SubmitGradientBtn.jsx";
 import validate from "../create/validate.js";
 import fileUpload from "../Publish/fileUpload.js";
 import humans from "../../assets/placeholder/humans_data.js";
@@ -15,9 +15,21 @@ import axios from "axios";
 import createToken from "./createToken.js";
 
 export default function Create() {
-  const tipos = ["Perro", "Gato", "Ave", "Conejo", "Caballo", "Hamster", "Tortuga", "Oveja", "Gallina", "Cerdo", "Otro"];
+  const tipos = [
+    "Perro",
+    "Gato",
+    "Ave",
+    "Conejo",
+    "Caballo",
+    "Hamster",
+    "Tortuga",
+    "Oveja",
+    "Gallina",
+    "Cerdo",
+    "Otro",
+  ];
   // const users = humans;
-  
+
   // console.log("Token link:", token.access);
 
   // const owner = users[1];
@@ -35,8 +47,8 @@ export default function Create() {
       .then((response) => {
         setOwner(response.data);
         console.log(owner.first_name, owner.email);
-        setToken(createToken("sergiomusta2", "sergioMusta123!"))
-        console.log(token)
+        setToken(createToken("sergiomusta2", "sergioMusta123!"));
+        console.log(token);
       })
       .catch((error) => {
         console.error("Error al obtener datos del dueño:", error);
@@ -90,9 +102,9 @@ export default function Create() {
   };
 
   const getIndex = (array, value) => {
-    let index = array.indexOf(value)
-    return index
-  }
+    let index = array.indexOf(value);
+    return index;
+  };
 
   const handleType = (e) => {
     console.log(e.target.value);
@@ -100,7 +112,7 @@ export default function Create() {
       ...prevErrors,
       ...validate("type", e.target.value),
     }));
-    let typeNumber = getIndex(tipos, e.target.value) + 1
+    let typeNumber = getIndex(tipos, e.target.value) + 1;
     error.petType ? console.log(error.petType) : setType(typeNumber);
   };
 
@@ -248,121 +260,118 @@ export default function Create() {
   return (
     <div>
       {/* esto podria ser un componente header  */}
-      <header className="flex my-8 items-center mx-6">
+      <header className='flex my-8 items-center mx-6'>
         <GoBackButton
-          className="rounded-3xl border border-dark-gray p-2 mr-8"
+          className='rounded-3xl border border-dark-gray p-2 mr-8'
           img={arrowLeftIcon}
-          alt="ícono volver atrás"
+          alt='ícono volver atrás'
         />
-        <h1 className="text-light-white font-custom text-sm text-center">
+        <h1 className='text-light-white font-custom text-sm text-center'>
           CREAR PERFIL MASKOTA
         </h1>
       </header>
-      <form action="" className="form mx-6 font-custom">
+      <form action='' className='form mx-6 font-custom'>
         <InputComp
-          label="Nombre de tu Maskota:"
-          type="text"
-          id="petName"
-          placeholder="Ingresá el nombre de tu maskota..."
+          label='Nombre de tu Maskota:'
+          type='text'
+          id='petName'
+          placeholder='Ingresá el nombre de tu maskota...'
           required={true}
           onChange={handleName}
         />
         {error.petName && (
-          <p className="text-xs font-medium text-white pt-2 font-custom">
+          <p className='text-xs font-medium text-white pt-2 font-custom'>
             {error.petName}
           </p>
         )}
 
-        <div className="my-3.5 flex flex-col">
-          <label htmlFor="type" className="text-white text-left font-semibold">
+        <div className='my-3.5 flex flex-col'>
+          <label htmlFor='type' className='text-white text-left font-semibold'>
             Tipo de maskota:
           </label>
           <select
-            name="type"
-            id="type"
-            className="w-40 mt-1.5 rounded-3xl h-9 p-2 text-white bg-dark-gray"
-            onChange={handleType}
-          >
-            <option key="default" value="default">
+            name='type'
+            id='type'
+            className='w-40 mt-1.5 rounded-3xl h-9 p-2 text-white bg-dark-gray'
+            onChange={handleType}>
+            <option key='default' value='default'>
               Seleccionar
             </option>
             {tipos.map((tipo) => (
-              <option key={tipo} value={tipo} className="">
+              <option key={tipo} value={tipo} className=''>
                 {tipo}
               </option>
             ))}
           </select>
           {error.petType && (
-            <p className="text-xs font-medium text-white pt-2 font-custom">
+            <p className='text-xs font-medium text-white pt-2 font-custom'>
               {error.petType}
             </p>
           )}
         </div>
         <InputComp
-          label="Nick de tu Maskota:"
-          type="text"
-          id="petNick"
-          placeholder="Elegí un nick único para tu maskota..."
+          label='Nick de tu Maskota:'
+          type='text'
+          id='petNick'
+          placeholder='Elegí un nick único para tu maskota...'
           required={true}
           onChange={handleNick}
         />
         {error.petNick && (
-          <p className="text-xs font-medium text-white pt-2 font-custom">
+          <p className='text-xs font-medium text-white pt-2 font-custom'>
             {error.petNick}
           </p>
         )}
-        <div className="dateContainer my-3.5 flex flex-col">
+        <div className='dateContainer my-3.5 flex flex-col'>
           <label
-            htmlFor="petDate"
-            className="text-white text-left font-semibold"
-          >
+            htmlFor='petDate'
+            className='text-white text-left font-semibold'>
             Fecha de nacimiento:
           </label>
           <input
-            type="date"
-            id="petDate"
+            type='date'
+            id='petDate'
             required={true}
-            className="w-40 mt-1.5 bg-dark-black text-white font-thin"
+            className='w-40 mt-1.5 bg-dark-black text-white font-thin'
             onChange={handleDate}
           />
           {error.petDate && (
-            <p className="text-xs font-medium text-white pt-2 font-custom">
+            <p className='text-xs font-medium text-white pt-2 font-custom'>
               {error.petDate}
             </p>
           )}
         </div>
-        <div className="my-3.5 flex flex-col">
+        <div className='my-3.5 flex flex-col'>
           <label
-            htmlFor="petProfile"
-            className="text-white text-left font-semibold"
-          >
+            htmlFor='petProfile'
+            className='text-white text-left font-semibold'>
             Agregar foto de perfil:
           </label>
-          <div className="relative overflow-hidden rounded-full w-36 h-36 border-solid border-2 p-1 m-auto mt-2 border-white">
+          <div className='relative overflow-hidden rounded-full w-36 h-36 border-solid border-2 p-1 m-auto mt-2 border-white'>
             <input
-              type="file"
-              id="petImg"
-              accept="image/*"
+              type='file'
+              id='petImg'
+              accept='image/*'
               required={false}
-              className="w-full h-full mt-1.5 absolute top-0 left-0 opacity-0 cursor-pointer z-1"
+              className='w-full h-full mt-1.5 absolute top-0 left-0 opacity-0 cursor-pointer z-1'
               onChange={handleImage}
             />
             {prevImg !== null || petProfile.petImg ? (
               <img
                 src={prevImg || petProfile.petImg}
-                alt="Perfil"
-                className="object-cover w-full h-full rounded-full"
+                alt='Perfil'
+                className='object-cover w-full h-full rounded-full'
               />
             ) : (
               <img
                 src={profilePlaceholder}
-                alt="Perfil Placeholder"
-                className="object-cover w-full h-full rounded-full"
+                alt='Perfil Placeholder'
+                className='object-cover w-full h-full rounded-full'
               />
             )}
           </div>
           {error.petImg && (
-            <p className="text-xs font-medium text-white pt-2 font-custom">
+            <p className='text-xs font-medium text-white pt-2 font-custom'>
               {error.petImg}
             </p>
           )}
@@ -371,7 +380,7 @@ export default function Create() {
           disabled={
             error.petName || error.petType || error.petNick || error.petDate
           }
-          BtnText="Crear Perfil"
+          BtnText='Crear Perfil'
           onClick={handleCreate}
           link={`/profile/2/${humanProfile ? humanProfile.name : ""}`}
         />
