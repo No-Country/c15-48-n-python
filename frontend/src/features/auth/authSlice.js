b0462d7699ca1c9a31dc0dcbe5123e987bcd9fd8
suspiciousRaccoon <spablocabral@gmail.com>
2023-12-26 22:52:03 -0300

    feat(authSlice): add username field to storage

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -2,11 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { refresh_token: null, token: null },
+  initialState: { username: null, refresh_token: null, token: null },
   reducers: {
-    setCredentials: (state, { payload: { refresh_token, token } }) => {
+    setCredentials: (
+      state,
+      { payload: { refresh_token, token, username } }
+    ) => {
       state.refresh_token = refresh_token;
       state.token = token;
+      state.username = username;
     },
   },
 });
